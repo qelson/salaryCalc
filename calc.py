8e13f657e36ef360f842508c1705a57ab2b5ee00
Quin A <qelson@protonmail.com>
2023-03-26 00:04:03 -0400

    first draft

---
+++ calc.py
@@ -0,0 +1,5 @@
+hourlyWage = float(input("Enter hourly wage: "))
+hoursPerWeek = float(input("Enter hours worked per week: "))
+weeksPerYear = float(input("Enter number of weeks worked per year: "))
+annualSalary = hourlyWage * hoursPerWeek * weeksPerYear
+print("Annual salary: $", annualSalary)
